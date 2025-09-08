4eb0f9bb89763e7378e21dd094d2f9ace1ec456e
Mohit Rao aka Halog3 <mohit.rao.333034@gmail.com>
2025-09-08 11:43:43 +0530

    string genral operations and revise strings

--- printlove.py
+++ printlove.py
@@ -1,1 +1,12 @@
-print('love ')
+import math 
+s = input('Enter the string: ')
+def allsubs(s):
+    n = len(s)
+    substring = set()
+    # generate all substing 
+    for i in range(n):
+        for j in range(i+1, n+1):
+            substring.add(s[i:j])
+    return list(substring)
+
+print(allsubs(s))
