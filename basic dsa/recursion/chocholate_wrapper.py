280490135d61db1309683f5629b621d195f50c1c
Mohit Rao aka Halog3 <mohit.rao.333034@gmail.com>
2025-09-30 16:28:54 +0530

    revision Day 1 some minor changes

--- basic dsa/recursion/chocholate_wrapper.py
+++ basic dsa/recursion/chocholate_wrapper.py
@@ -1,6 +1,8 @@
-'''Given three values money, price, and wrap, the task is to find the total number of chocolates you can eat.
+'''
+Given three values money, price, and wrap, the task is to find the total number of chocolates you can eat.
  Here money defines the total amount of money you have, price is the cost of buying a single chocolate and wrap
-   defines the number of wrappers that can be returned to get one extra chocolate.'''
+   defines the number of wrappers that can be returned to get one extra chocolate.
+'''
 
 def count(money ,price, wrap):
     if money <price:
--- printlove.py
+++ printlove.py
@@ -1,12 +1,3 @@
-import math 
-s = input('Enter the string: ')
-def allsubs(s):
-    n = len(s)
-    substring = set()
-    # generate all substing 
-    for i in range(n):
-        for j in range(i+1, n+1):
-            substring.add(s[i:j])
-    return list(substring)
-
-print(allsubs(s))
+n = 345435435
+res = list(map(int, str(n))) 
+print(res)
